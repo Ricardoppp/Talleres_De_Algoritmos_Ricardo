c07106dc9e09ee2e60af2d5eee7cf70d3e865032
Ricardo Prieto <ricardo_19_pr@hotmail.com>
2022-02-05 14:11:10 -0500

    Punto 1
    Punto #1 del taller de control secuenciales

---
+++ punto_uno.py
@@ -0,0 +1,20 @@
+print("Punto Uno del Taller ")
+
+"""
+
+datos de una entrada
+edad_uno-->e_uno-->int
+edad_dos-->e_dos-->int
+edad_tres -->e_tres-->int
+
+"""
+
+#entradas 
+e_uno=int(input("digite edad uno "))
+e_dos=int(input("digite edad dos "))
+e_tres=int(input("digite edad tres "))
+
+#caja negra
+p=(e_uno+e_dos+e_tres)/3 #float 
+#salidas
+print(f"el promedio de edad es {int(p)}")

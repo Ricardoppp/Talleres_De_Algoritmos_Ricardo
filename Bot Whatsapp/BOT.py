b8ebd9d489047632fc30ed8f093b889df1f5dd83
Ricardo Prieto <ricardo_19_pr@hotmail.com>
2022-02-08 21:51:24 -0500

    Bot Whatsapp

---
+++ Bot Whatsapp/BOT.py
@@ -0,0 +1,18 @@
+import pyautogui, webbrowser
+import webbrowser as web
+from time import sleep
+
+webbrowser.open("https://web.whatsapp.com/send?phone=+573053888810")
+sleep(10)
+"""
+
+for i in range (10)
+#pyautogui.typewrite ("hola como estas")
+#pyautogui.press("enter")
+
+"""
+
+with open("mensaje.txt", "r") as file:
+    for line in file:
+        pyautogui.typewrite(line)
+        pyautogui.press("enter")
